e791d9978bb125400ecd009c6442a4b8616525cc
Kory Skarbek and grinil khanna <kory+grinil@>
2017-02-08 10:15:43 -0800

    rectangle checker complete

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,28 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace RectangleChecker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["rectangle_form.cshtml"];
+      };
+      Get["/rectangle_result"] = _ => {
+        Dictionary<string, object> Shapes = new Dictionary<string, object>();
+        Rectangle myRectangle = new Rectangle(Request.Query["side-length"], Request.Query["side-width"]);
+        Shapes.Add("ResultingRectangle", myRectangle);
+
+        if (myRectangle.IsSquare())
+        {
+          Cube myCube = new Cube(myRectangle);
+          Shapes.Add("ResultingCube", myCube);
+        }
+
+        return View["rectangle_result.cshtml", Shapes];
+      };
+    }
+  }
+}
